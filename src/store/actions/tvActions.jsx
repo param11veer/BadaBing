a192d2499f7f3c11a95fa6110211b69a29ce2adf
Paramveer Singh Udawat <param11veerudawat@gmail.com>
2025-03-30 18:30:49 +0530

    second

--- src/store/actions/tvActions.jsx
+++ src/store/actions/tvActions.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from "../../utils/axios";
-import { loadtv } from "../reducers/tvslice";
+import { loadtv } from "../reducers/tvSlice";
 
 const tvActions = () => {
   return (
